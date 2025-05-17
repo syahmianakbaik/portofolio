034cdf8aa0a5af474e555bf32ad2df06ba0d3e8c
syahmianakbaik <fsyahmi527@gmail.com>
2025-05-17 18:50:41 +0700

    idk time im update

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,5 +13,8 @@ export default defineConfig({
     vite: {
         plugins: [tailwindcss()]
     },
-    integrations: [mdx(), sitemap()]
+    integrations: [mdx(), sitemap()],
+    experimental: {
+        session: true,
+    },
 });
